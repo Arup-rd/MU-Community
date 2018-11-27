2f15aa025857c1fb6cd48d891ebd0942efc4402e
Arup rd <33912024+Arup-rd@users.noreply.github.com>
2018-11-28 00:53:36 +0600

    Store updated

--- client/src/store.js
+++ client/src/store.js
@@ -1,17 +1,23 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import { createLogger } from 'redux-logger';
 
 const initialState = {};
+
+const logger = createLogger({
+  collapsed: true,
+  diff: true
+});
 
 const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(
+    applyMiddleware(...middleware, logger)
   )
 );
 
